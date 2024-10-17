07c9ef07a837c16caa46ecb413356e0e8581116d
OLIN10-hub <muriuolin8@gmail.com>
2024-10-18 02:39:12 +0300

    added heading and services offered

---
+++ client/src/components/OurServices.js
@@ -0,0 +1,22 @@
+// OurServices.js
+import React from 'react';
+
+const OurServices = () => {
+    return (
+        <div className="our-services">
+            <h1>Our Services</h1>
+            <ul>
+                <li>Service 1: Description of Service 1.</li>
+                <li>Service 2: Description of Service 2.</li>
+                <li>Service 3: Description of Service 3.</li>
+                <li>Service 4: Description of Service 4.</li>
+            </ul>
+            <p>
+                We tailor our services to meet the unique needs of each client,
+                ensuring a personalized experience every time.
+            </p>
+        </div>
+    );
+};
+
+export default OurServices;
