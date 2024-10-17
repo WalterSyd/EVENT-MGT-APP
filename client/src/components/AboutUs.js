a415dafecb6d20018de419afe869249c660d0e0c
OLIN10-hub <muriuolin8@gmail.com>
2024-10-18 00:56:39 +0300

    created about function

---
+++ client/src/components/AboutUs.js
@@ -0,0 +1,18 @@
+// AboutUs.js
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div className="about-us">
+            <h1> </h1>
+            <p>
+               
+            </p>
+            <p>
+                
+            </p>
+        </div>
+    );
+};
+
+export default AboutUs;
